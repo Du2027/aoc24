2b896f3c03cc2e85ee463ae1200ca83574c6fb2e
P.-L. <paul-luca.k@proton.me>
2024-12-02 19:13:52 +0100

    changes, somehow still doesnt work

--- 02.12/main.c
+++ 02.12/main.c
@@ -1,81 +1,99 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stdbool.h>
+   #include <stdio.h>
+   #include <stdlib.h>
+   #include <string.h>
 
-int main(int argc, char** argv){
-    FILE* file = fopen("input.txt", "r");
-    int levels[1000][30];
-    char line[50];
-    int iteration = 0;
-    int level_iteration = 0;
-    int level_array_iteration = 0;
-    char next;
-    char buffer[3];
+   int main(int argc, char **argv) {
+   FILE *file = fopen("input.txt", "r");
+   int levels[1000][30] = {0};
+   char line[50];
 
-    while(fgets(line, 50, file) != NULL){
-        next = line[0];
-        while (next == 32 || next <= '9' && next >= '0') {
-            buffer[0] = line[level_iteration];
-            if(buffer[1] != 32){buffer[1] = line[level_iteration + 1];}
-            levels[iteration][level_array_iteration] = atoi(buffer);
-            level_iteration += 3;
-            level_array_iteration++;
-        }
-        level_iteration = 0;
-        level_array_iteration = 0;
-        iteration++;
-    }
+   char *token;
+   short int ycounter = 0;
+   short int xcounter = 0;
 
-    /*for (int i = 0; i < 100; i++) {
-        for (int n = 0; n < 30; n++) {
-        }
-        printf("\n");
-        }*/
+   while (fgets(line, 50, file) != NULL) {
+      token = strtok(line, " ");
+      xcounter = 0;
+      while (token != NULL) {
+          //printf("%s@%d-%d a:%d", token, ycounter, xcounter, atoi(token));
+          levels[ycounter][xcounter] = atoi(token);
+          token = strtok(NULL, " ");
+          xcounter++;
+      }
+      ycounter++;
+   }
 
-    iteration = 0;
-    int safe_counter = 0;
-    int diff = 0;
-    bool contin = true;
-    int bigger = 0;
-    for (int i = 0; i < 1000; i++) {
-            if(levels[i][0] < levels[i][1]){
-                bigger = -1;
+   for (int i = 0; i < 1000; i++) {
+      for (int n = 0; n < 30; n++) {
+         printf("%d, ", levels[i][n]);
+      }
+      printf("\n");
+   }
+
+   int iteration = 0;
+   int safe_counter = 0;
+   int diff = 0;
+   bool contin = true;
+   int bigger = 0;
+   short int problemc = 0;
+   short int n = 0;
+   short int avg = 0;
+
+   for (int i = 0; i < 1000; i++) {
+      while (levels[i][n + 1] != 0){
+         diff = levels[i][n] - levels[i][n + 1];
+         if (diff < 0) {
+            avg--;
+         }
+         if (diff > 0) {
+            avg++;
+         }
+         n++;
+      }
+
+      if (avg <= 0) {
+         bigger = -1;
+      }
+      else if (avg > 0) {
+         bigger = 1;
+      }
+
+      n = 0;
+      avg = 0;
+
+      while (levels[i][iteration + 1] != 0) {
+         diff = levels[i][iteration] - levels[i][iteration + 1];
+         if (abs(diff) > 3 || abs(diff) == 0) {
+            problemc++;
+            contin = false;
+         }
+
+            if (levels[i][iteration] < levels[i][iteration + 1] && bigger == -1) {
+
             }
-            else if(levels[i][0] > levels[i][1]){
-                bigger = 1;
-            }
-        while (levels[i][iteration] != 0) {
-            printf("%d ",levels[i][iteration]);
-            iteration++;
+            else if (levels[i][iteration] > levels[i][iteration + 1] && bigger == 1) {
 
-
-            diff = levels[i][iteration] - levels[i][iteration + 1];
-            if(diff < 0){diff = diff * -1;}
-            if(diff > 3){
-                break;
-            }
-            if(levels[i][iteration] < levels[i][iteration +1] && bigger == -1){
-                printf("%d < %d\n", levels[i][iteration], levels[i][iteration +1]);
-            }
-            else if(levels[i][iteration] > levels[i][iteration +1] && bigger == 1){
-                printf("%d > %d\n", levels[i][iteration], levels[i][iteration +1]);
             }
             else {
-                contin = false;
-                printf("WRONG - %d, %d\n", levels[i][iteration], levels[i][iteration +1]);
+               contin = false;
+               problemc++;
+               //printf("WRONG - %d, %d\n", levels[i][iteration], levels[i][iteration +1]);
             }
-        }
-        if (contin == true) {
-            safe_counter++;
-        }
-        else {
-            contin = true;
-        }
-        iteration = 0;
-        printf("\n");
-    }
-    printf("safec %d", safe_counter);
 
-    return 0;
+         iteration++;
+      }
+      if (contin == true || problemc == 1) {
+         safe_counter++;
+      }
+      else {
+         contin = true;
+      }
+      problemc = 0;
+      iteration = 0;
+   }
+
+   printf("safec %d", safe_counter);
+   fclose(file);
+   return 0;
 }
